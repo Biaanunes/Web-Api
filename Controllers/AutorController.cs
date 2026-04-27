55b65d0bf02897b6902cbac4d2e30de4a61d2ca6
Biaanunes <bianunesdeveras@gmail.com>
2026-04-27 18:24:44 -0300

    Update AutorController.cs

--- Controllers/AutorController.cs
+++ Controllers/AutorController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI8_Video.DTO.Autor;
+using WebAPI8_Video.Models;
+using WebAPI8_Video.Services.Autor;
 
 namespace WebAPI8_Video.Controllers
 {
@@ -7,5 +10,60 @@ namespace WebAPI8_Video.Controllers
     [ApiController]
     public class AutorController : ControllerBase
     {
-    }
+        private readonly IAutorInterface _autorInterface;
+        public AutorController(IAutorInterface autorInterface)
+        {
+            _autorInterface = autorInterface;
+        }
+
+        [HttpGet("ListarAutores")]
+        public async Task<ActionResult<responseModel<List<autorModel>>>> ListarAutores()
+        {
+            var autores = await _autorInterface.ListarAutores();
+            return Ok(autores);
+
+        }
+
+        [HttpGet("BuscarAutorPorId/{idAutor}")]
+        public async Task<ActionResult<responseModel<autorModel>>> BuscarAutorPorId(int idAutor)
+        {
+            var autor = await _autorInterface.BuscarAutorPorId(idAutor);
+            return Ok(autor);
+
+        }
+
+        [HttpGet("BuscarAutorPorIdLivro/{idLivro}")]
+        public async Task<ActionResult<responseModel<autorModel>>> BuscarAutorPorIdLivro(int idLivro)
+        {
+            var autor = await _autorInterface.BuscarAutorPorIdLivro(idLivro);
+            return Ok(autor);
+
+        }
+
+        [HttpPost("CriarAutor")]
+
+        public async Task<ActionResult<responseModel<List<autorModel>>>> CriarAutor(AutorCriacaoDto autorCriacaoDto)
+        {
+            var autores = await _autorInterface.CriarAutor(autorCriacaoDto);
+            return Ok(autores);
+
+        }
+
+        [HttpPut("EditarAutor")]
+        public async Task<ActionResult<responseModel<List<autorModel>>>> EditarAutor(AutorEdicaoDto autorEdicaoDto)
+        {
+            var autores = await _autorInterface.EditarAutor(autorEdicaoDto);
+            return Ok(autores);
+
+        }
+
+        [HttpDelete("ExcluirAutor")]
+        public async Task<ActionResult<responseModel<List<autorModel>>>> ExcluirAutor(int idAutor)
+        {
+            var autores = await _autorInterface.ExcluirAutor(idAutor);
+            return Ok(autores);
+
+        }
+
+    };
 }
