a84077dac045f2a5c1deb30c59d4b61bd644bdc4
Biaanunes <bianunesdeveras@gmail.com>
2026-04-27 18:27:04 -0300

    Update ILivroInterface.cs

--- Livro/ILivroInterface.cs
+++ Livro/ILivroInterface.cs
@@ -1,6 +1,16 @@
-﻿namespace WebAPI8_Video.Services.Livro
+using WebAPI8_Video.DTO.Autor;
+using WebAPI8_Video.DTO.LivroDto;
+using WebAPI8_Video.Models;
+
+namespace WebAPI8_Video.Services.Livro
 {
-    public class ILivroInterface
+    public interface ILivroInterface
     {
+        Task<responseModel<List<LivroModel>>> ListarLivros();
+        Task<responseModel<LivroModel>> BuscarLivroPorId(int idLivro);
+        Task<responseModel<LivroModel>> BuscarLivroPorIdAutor(int idLivro);
+        Task<responseModel<List<LivroModel>>> Criarlivro(LivroCriacaoDto livroCriacaoDto);
+        Task<responseModel<List<LivroModel>>> EditarLivro(LivroEdicaoDto livroEdicaoDto);
+        Task<responseModel<List<LivroModel>>> ExcluirLivro(int idLivro);
     }
 }
