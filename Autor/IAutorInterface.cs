db2c69c5c87d5ce4ba3bda2a542378cd7956dc92
Biaanunes <bianunesdeveras@gmail.com>
2026-04-27 18:24:17 -0300

    Update IAutorInterface.cs

--- Autor/IAutorInterface.cs
+++ Autor/IAutorInterface.cs
@@ -1,6 +1,16 @@
-﻿namespace WebAPI8_Video.Services.Autor
+using WebAPI8_Video.DTO.Autor;
+using WebAPI8_Video.Models;
+
+namespace WebAPI8_Video.Services.Autor
 {
     public interface IAutorInterface
     {
+        Task<responseModel<List<autorModel>>> ListarAutores();
+        Task<responseModel<autorModel>> BuscarAutorPorId(int idAutor);
+        Task<responseModel<autorModel>> BuscarAutorPorIdLivro(int idLivro);
+        Task<responseModel<List<autorModel>>> CriarAutor(AutorCriacaoDto autorCriacaoDto);
+        Task<responseModel<List<autorModel>>> EditarAutor(AutorEdicaoDto autorEdicaoDto);
+        Task<responseModel<List<autorModel>>> ExcluirAutor(int idAutor);
+       
     }
 }
