34c278935659ca1e9fb94bc8131eddac1fd1a6c5
Biaanunes <bianunesdeveras@gmail.com>
2026-04-27 18:28:34 -0300

    Update autorModel.cs

--- Models/autorModel.cs
+++ Models/autorModel.cs
@@ -1,13 +1,13 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 
 namespace WebAPI8_Video.Models
 {
     public class autorModel
     {
         public int Id { get; set; }
-        public required string nome { get; set; }
-        public required string Sobrenome { get; set; }
-        [JsonIgnore]
+        public string nome { get; set; }
+        public string Sobrenome { get; set; }
+        [JsonIgnore]    
         public ICollection<LivroModel> Livros { get; set; }
     }
 }
