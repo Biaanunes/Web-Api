1ec850265e0b2bb44fb6856cef0635e5bd79f46c
Biaanunes <bianunesdeveras@gmail.com>
2026-04-27 18:26:02 -0300

    Update LivroCriacaoDto.cs

--- DTO/LivroDto/LivroCriacaoDto.cs
+++ DTO/LivroDto/LivroCriacaoDto.cs
@@ -1,6 +1,10 @@
-﻿namespace WebAPI8_Video.LivroDto
+using WebAPI8_Video.Models;
+
+namespace WebAPI8_Video.DTO.LivroDto
 {
     public class LivroCriacaoDto
     {
+        public string Titulo { get; set; }
+        public autorModel autor { get; set; }
     }
 }
